19ea5380970732cd6081d05e06432feb4850c63a
amercader <amercadero@gmail.com>
2012-08-15 18:28:08 +0100

    [#2852,#2853] Reword errors

--- ckanext/harvest/controllers/view.py
+++ ckanext/harvest/controllers/view.py
@@ -230,8 +230,14 @@ class ViewController(BaseController):
         except NotAuthorized,e:
             abort(401,self.not_auth_message)
         except Exception, e:
-            msg = 'An error occurred: [%s]' % e.message
-            h.flash_error(msg)
+            if 'Can not create jobs on inactive sources' in str(e):
+                h.flash_error('Cannot create new harvest jobs on inactive sources. '
+                + 'Please first change the status to \'active\'')
+            elif 'There already is an unrun job for this source' in str(e):
+                h.flash_error('A harvest job has already been scheduled for this source')
+            else:
+                msg = 'An error occurred: [%s]' % str(e)
+                h.flash_error(msg)
 
         redirect(h.url_for('harvest'))
 
