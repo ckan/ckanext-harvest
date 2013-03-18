cb80ac784e8d0851cf16e0083b4e384bacf034a8
amercader <amercadero@gmail.com>
2013-03-18 16:29:29 +0000

    Add logic to show private datasets to authorizaded users

--- ckanext/harvest/helpers.py
+++ ckanext/harvest/helpers.py
@@ -27,6 +27,12 @@ def package_list_for_source(source_id):
     }
 
     context = {'model': model, 'session': model.Session}
+
+    user_member_of_orgs = [org['id'] for org
+                   in h.organizations_available('read')]
+    if (p.toolkit.c.harvest_source and p.toolkit.c.harvest_source['owner_org'] in user_member_of_orgs):
+        context['ignore_capacity_check'] = True
+
     query = logic.get_action('package_search')(context, search_dict)
 
     base_url = h.url_for('{0}_read'.format(DATASET_TYPE_NAME), id=source_id)
