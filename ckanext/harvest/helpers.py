6f009c22b4e72d6739260549a8ecbb6bf4a8b3e2
Mikko Koho <mikko.koho@csc.fi>
2013-11-21 14:14:56 +0200

    Put harvest_source_id in quotes to prevent Solr errors

--- ckanext/harvest/helpers.py
+++ ckanext/harvest/helpers.py
@@ -18,7 +18,7 @@ def package_list_for_source(source_id):
     '''
     limit = 20
     page = int(request.params.get('page', 1))
-    fq = 'harvest_source_id:{0}'.format(source_id)
+    fq = 'harvest_source_id:"{0}"'.format(source_id)
     search_dict = {
         'fq' : fq,
         'rows': limit,
