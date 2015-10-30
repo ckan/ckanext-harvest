3f37ae5f45c8f4ded7e587f746273bf99cd691cd
Mark Winterbottom <mark@winterbottom.me>
2015-10-30 16:11:25 +0000

    Corrected docstring.

--- ckanext/harvest/logic/action/delete.py
+++ ckanext/harvest/logic/action/delete.py
@@ -14,12 +14,6 @@ def harvest_source_delete(context, data_dict):
     which will delete the actual harvest type dataset and the
     HarvestSource object (via the after_delete extension point).
 
-    :param id: the name or id of the harvest source to delete
-    :type id: string
-
-    :returns: the newly created harvest source
-    :rtype: dictionary
-
     """
 
     log.info('Deleting harvest source: %r', data_dict)
