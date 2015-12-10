8f54421c99ab278c4e1723abbb0a7494c32f0c38
David Read <david.read@hackneyworkshop.com>
2015-12-10 16:11:56 +0000

    [#214] ckan.legacy_templates=True is really harmless, so just warn if it is set.

--- ckanext/harvest/plugin.py
+++ ckanext/harvest/plugin.py
@@ -232,9 +232,10 @@ class Harvest(p.SingletonPlugin, DefaultDatasetForm, DefaultTranslation):
         if not p.toolkit.check_ckan_version(min_version='2.0'):
             assert 0, 'CKAN before 2.0 not supported by ckanext-harvest - '\
                 'genshi templates not supported any more'
-        assert not p.toolkit.asbool(config.get('ckan.legacy_templates',
-                                               False)),\
-            'Old genshi templates not supported any more by ckanext-harvest'
+        if p.toolkit.asbool(config.get('ckan.legacy_templates', False)):
+            log.warn('Old genshi templates not supported any more by '
+                     'ckanext-harvest so you should set ckan.legacy_templates '
+                     'option to True any more.')
         p.toolkit.add_template_directory(config, 'templates')
         p.toolkit.add_public_directory(config, 'public')
         p.toolkit.add_resource('fanstatic_library', 'ckanext-harvest')
