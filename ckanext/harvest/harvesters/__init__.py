3aec5ed279c9b84f410b4355a10245b37b5c9b72
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-03-06 13:36:42 +0200

    Fix importing, so that extensions will not break

--- ckanext/harvest/harvesters/__init__.py
+++ ckanext/harvest/harvesters/__init__.py
@@ -1,6 +1,4 @@
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    import pkgutil
-    __path__ = pkgutil.extend_path(__path__, __name__)
+from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
+from ckanext.harvest.harvesters.base import HarvesterBase
+
+__all__ = ['CKANHarvester', 'HarvesterBase']
