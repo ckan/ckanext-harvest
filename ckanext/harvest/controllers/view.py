ckan/ckanext-harvest

commit ed288dca63ffc6b2c3052c8e4aa3f14bf0525f9e

Reduce usage of c vars, add more tests

ckanext/harvest/controllers/view.py
@@ -1,11 +1,13 @@
 import re
 import xml.etree.ElementTree as etree
+
 try:
     # Python 2.7
     xml_parser_exception = etree.ParseError
 except AttributeError:
     # Python 2.6
     from xml.parsers import expat
+
     xml_parser_exception = expat.ExpatError
 
 from pylons.i18n import _
@@ -14,14 +16,15 @@ from ckan import model
 
 import ckan.plugins as p
 import ckan.lib.helpers as h, json
-from ckan.lib.base import BaseController, c, \
-                          request, response, render, abort
+from ckan.lib.base import BaseController, c, request, response, render, abort
 
 from ckanext.harvest.logic import HarvestJobExists, HarvestSourceInactiveError
 from ckanext.harvest.plugin import DATASET_TYPE_NAME
 
 import logging
+
 log = logging.getLogger(__name__)
+
 
 class ViewController(BaseController):
 
@@ -29,17 +32,20 @@ class ViewController(BaseController):
 
     def __before__(self, action, **params):
 
-        super(ViewController,self).__before__(action, **params)
+        super(ViewController, self).__before__(action, **params)
 
         c.dataset_type = DATASET_TYPE_NAME
 
-    def delete(self,id):
+    def delete(self, id):
         try:
-            context = {'model':model, 'user':c.user}
+            context = {'model': model, 'user': c.user}
 
-            context['clear_source'] = request.params.get('clear', '').lower() in (u'true', u'1')
+            context['clear_source'] = request.params.get('clear', '').lower() in (
+                u'true',
+                u'1',
+            )
 
-            p.toolkit.get_action('harvest_source_delete')(context, {'id':id})
+            p.toolkit.get_action('harvest_source_delete')(context, {'id': id})
 
             if context['clear_source']:
                 h.flash_success(_('Harvesting source successfully cleared'))
@@ -48,44 +54,51 @@ class ViewController(BaseController):
 
             h.redirect_to(h.url_for('{0}_admin'.format(DATASET_TYPE_NAME), id=id))
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest source not found'))
+            abort(404, _('Harvest source not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-
+            abort(401, self.not_auth_message)
 
     def refresh(self, id):
         try:
-            context = {'model':model, 'user':c.user, 'session':model.Session}
+            context = {'model': model, 'user': c.user, 'session': model.Session}
             p.toolkit.get_action('harvest_job_create')(
-                context, {'source_id': id, 'run': True})
-            h.flash_success(_('Harvest will start shortly. Refresh this page for updates.'))
+                context, {'source_id': id, 'run': True}
+            )
+            h.flash_success(
+                _('Harvest will start shortly. Refresh this page for updates.')
+            )
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest source not found'))
+            abort(404, _('Harvest source not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-        except HarvestSourceInactiveError, e:
-            h.flash_error(_('Cannot create new harvest jobs on inactive '
-                            'sources. First, please change the source status '
-                            'to \'active\'.'))
-        except HarvestJobExists, e:
-            h.flash_notice(_('A harvest job has already been scheduled for '
-                             'this source'))
-        except Exception, e:
-                msg = 'An error occurred: [%s]' % str(e)
-                h.flash_error(msg)
+            abort(401, self.not_auth_message)
+        except HarvestSourceInactiveError:
+            h.flash_error(
+                _(
+                    'Cannot create new harvest jobs on inactive '
+                    'sources. First, please change the source status '
+                    'to "active".'
+                )
+            )
+        except HarvestJobExists:
+            h.flash_notice(
+                _('A harvest job has already been scheduled for ' 'this source')
+            )
+        except Exception as e:
+            msg = 'An error occurred: [%s]' % str(e)
+            h.flash_error(msg)
 
         h.redirect_to(h.url_for('{0}_admin'.format(DATASET_TYPE_NAME), id=id))
 
     def clear(self, id):
         try:
-            context = {'model':model, 'user':c.user, 'session':model.Session}
-            p.toolkit.get_action('harvest_source_clear')(context,{'id':id})
+            context = {'model': model, 'user': c.user, 'session': model.Session}
+            p.toolkit.get_action('harvest_source_clear')(context, {'id': id})
             h.flash_success(_('Harvest source cleared'))
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest source not found'))
+            abort(404, _('Harvest source not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-        except Exception, e:
+            abort(401, self.not_auth_message)
+        except Exception as e:
             msg = 'An error occurred: [%s]' % str(e)
             h.flash_error(msg)
 
@@ -94,11 +107,13 @@ class ViewController(BaseController):
     def show_object(self, id, ref_type='object'):
 
         try:
-            context = {'model':model, 'user':c.user}
+            context = {'model': model, 'user': c.user}
             if ref_type == 'object':
                 obj = p.toolkit.get_action('harvest_object_show')(context, {'id': id})
             elif ref_type == 'dataset':
-                obj = p.toolkit.get_action('harvest_object_show')(context, {'dataset_id': id})
+                obj = p.toolkit.get_action('harvest_object_show')(
+                    context, {'dataset_id': id}
+                )
 
             # Check content type. It will probably be either XML or JSON
             try:
@@ -108,11 +123,13 @@ class ViewController(BaseController):
                 elif 'original_document' in obj['extras']:
                     content = obj['extras']['original_document']
                 else:
-                    abort(404,_('No content found'))
+                    abort(404, _('No content found'))
                 try:
-                    etree.fromstring(re.sub('<\?xml(.*)\?>','',content))
+                    etree.fromstring(re.sub('<\?xml(.*)\?>', '', content))
                 except UnicodeEncodeError:
-                    etree.fromstring(re.sub('<\?xml(.*)\?>','',content.encode('utf-8')))
+                    etree.fromstring(
+                        re.sub('<\?xml(.*)\?>', '', content.encode('utf-8'))
+                    )
                 response.content_type = 'application/xml; charset=utf-8'
                 if not '<?xml' in content.split('\n')[0]:
                     content = u'<?xml version="1.0" encoding="UTF-8"?>\n' + content
@@ -127,90 +144,104 @@ class ViewController(BaseController):
 
             response.headers['Content-Length'] = len(content)
             return content.encode('utf-8')
-        except p.toolkit.ObjectNotFound, e:
-            abort(404,_(str(e)))
+        except p.toolkit.ObjectNotFound as e:
+            abort(404, _(str(e)))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-        except Exception, e:
+            abort(401, self.not_auth_message)
+        except Exception as e:
             msg = 'An error occurred: [%s]' % str(e)
-            abort(500,msg)
-
+            abort(500, msg)
 
     def _get_source_for_job(self, source_id):
 
         try:
             context = {'model': model, 'user': c.user}
-            source_dict = p.toolkit.get_action('harvest_source_show')(context,
-                    {'id': source_id})
+            source_dict = p.toolkit.get_action('harvest_source_show')(
+                context, {'id': source_id}
+            )
         except p.toolkit.ObjectNotFound:
             abort(404, p.toolkit._('Harvest source not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-        except Exception, e:
+            abort(401, self.not_auth_message)
+        except Exception as e:
             msg = 'An error occurred: [%s]' % str(e)
-            abort(500,msg)
+            abort(500, msg)
 
         return source_dict
 
     def show_job(self, id, source_dict=False, is_last=False):
 
         try:
-            context = {'model':model, 'user':c.user}
-            c.job = p.toolkit.get_action('harvest_job_show')(context, {'id': id})
-            c.job_report = p.toolkit.get_action('harvest_job_report')(context, {'id': id})
+            context = {'model': model, 'user': c.user}
+            job = p.toolkit.get_action('harvest_job_show')(context, {'id': id})
+            job_report = p.toolkit.get_action('harvest_job_report')(
+                context, {'id': id}
+            )
 
             if not source_dict:
-                source_dict = p.toolkit.get_action('harvest_source_show')(context, {'id': c.job['source_id']})
+                source_dict = p.toolkit.get_action('harvest_source_show')(
+                    context, {'id': job['source_id']}
+                )
 
-            c.harvest_source = source_dict
-            c.is_last_job = is_last
-
-            return render('source/job/read.html')
+            return render(
+                'source/job/read.html',
+                extra_vars={
+                    'harvest_source': source_dict,
+                    'job': job,
+                    'job_report': job_report,
+                    'is_last_job': is_last,
+                },
+            )
 
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest job not found'))
+            abort(404, _('Harvest job not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-        except Exception, e:
+            abort(401, self.not_auth_message)
+        except Exception as e:
             msg = 'An error occurred: [%s]' % str(e)
-            abort(500,msg)
+            abort(500, msg)
 
     def about(self, id):
         try:
-            context = {'model':model, 'user':c.user}
-            c.harvest_source = p.toolkit.get_action('harvest_source_show')(context, {'id':id})
-            return render('source/about.html')
+            context = {'model': model, 'user': c.user}
+            harvest_source = p.toolkit.get_action('harvest_source_show')(
+                context, {'id': id}
+            )
+            return render(
+                'source/about.html', extra_vars={'harvest_source': harvest_source}
+            )
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest source not found'))
+            abort(404, _('Harvest source not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
+            abort(401, self.not_auth_message)
 
     def admin(self, id):
         try:
-            context = {'model':model, 'user':c.user}
+            context = {'model': model, 'user': c.user}
             p.toolkit.check_access('harvest_source_update', context, {'id': id})
-            c.harvest_source = p.toolkit.get_action('harvest_source_show')(context, {'id':id})
-            return render('source/admin.html')
+            harvest_source = p.toolkit.get_action('harvest_source_show')(
+                context, {'id': id}
+            )
+            return render(
+                'source/admin.html', extra_vars={'harvest_source': harvest_source}
+            )
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest source not found'))
+            abort(404, _('Harvest source not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-                
+            abort(401, self.not_auth_message)
+
     def abort_job(self, source, id):
         try:
-            context = {'model':model, 'user':c.user}
-            c.job = p.toolkit.get_action('harvest_job_abort')(context, {'id': id})
-            c.harvest_source = p.toolkit.get_action('harvest_source_show')(context, {'id': source})
             h.flash_success(_('Harvest job stopped'))
-            
+
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest job not found'))
+            abort(404, _('Harvest job not found'))
         except p.toolkit.NotAuthorized:
-            abort(401,self.not_auth_message)
-        except Exception, e:
+            abort(401, self.not_auth_message)
+        except Exception as e:
             msg = 'An error occurred: [%s]' % str(e)
-            abort(500,msg)
-            
+            abort(500, msg)
+
         h.redirect_to(h.url_for('{0}_admin'.format(DATASET_TYPE_NAME), id=source))
 
     def show_last_job(self, source):
@@ -220,24 +251,32 @@ class ViewController(BaseController):
         if not source_dict['status']['last_job']:
             abort(404, _('No jobs yet for this source'))
 
-        return self.show_job(source_dict['status']['last_job']['id'],
-                             source_dict=source_dict,
-                             is_last=True)
-
+        return self.show_job(
+            source_dict['status']['last_job']['id'],
+            source_dict=source_dict,
+            is_last=True,
+        )
 
     def list_jobs(self, source):
 
         try:
-            context = {'model':model, 'user':c.user}
-            c.harvest_source =  p.toolkit.get_action('harvest_source_show')(context, {'id': source})
-            c.jobs = p.toolkit.get_action('harvest_job_list')(context, {'source_id': c.harvest_source['id']})
+            context = {'model': model, 'user': c.user}
+            harvest_source = p.toolkit.get_action('harvest_source_show')(
+                context, {'id': source}
+            )
+            jobs = p.toolkit.get_action('harvest_job_list')(
+                context, {'source_id': harvest_source['id']}
+            )
 
-            return render('source/job/list.html')
+            return render(
+                'source/job/list.html',
+                extra_vars={'harvest_source': harvest_source, 'jobs': jobs},
+            )
 
         except p.toolkit.ObjectNotFound:
-            abort(404,_('Harvest source not found'))
-        except p.toolkit.NotAuthorized, e:
-            abort(401,self.not_auth_message)
-        except Exception, e:
+            abort(404, _('Harvest source not found'))
+        except p.toolkit.NotAuthorized:
+            abort(401, self.not_auth_message)
+        except Exception as e:
             msg = 'An error occurred: [%s]' % str(e)
-            abort(500,msg)
+            abort(500, msg)

ckanext/harvest/fanstatic_library/styles/harvest.css
@@ -42,5 +42,4 @@ header.with-filter h1 {
 
 #source-new.bs2 .control-label {
   width: 125px;
--}
-
+}

ckanext/harvest/helpers.py
@@ -8,7 +8,22 @@ from ckanext.harvest.model import UPDATE_FREQUENCIES
 from ckanext.harvest.plugin import DATASET_TYPE_NAME
 from ckanext.harvest.interfaces import IHarvester
 
+
+c = p.toolkit.c
 request = p.toolkit.request
+
+
+def get_harvest_source(source_id=None):
+
+    context = {'model': model, 'session': model.Session}
+    if source_id:
+        return p.toolkit.get_action('harvest_source_show')(context, {'id': source_id})
+    elif hasattr(c, 'pkg_dict'):
+        return c.pkg_dict
+    elif hasattr(c, 'pkg'):
+        return p.toolkit.get_action('harvest_source_show')(context, {'id': c.pkg.id})
+
+    return None
 
 
 def package_list_for_source(source_id):
@@ -29,12 +44,12 @@ def package_list_for_source(source_id):
     }
 
     context = {'model': model, 'session': model.Session}
-
-    owner_org =  p.toolkit.c.harvest_source.get('owner_org', '')
+    harvest_source = get_harvest_source(source_id)
+    owner_org = harvest_source.get('owner_org', '')
     if owner_org:
         user_member_of_orgs = [org['id'] for org
                    in h.organizations_available('read')]
-        if (p.toolkit.c.harvest_source and owner_org in user_member_of_orgs):
+        if (harvest_source and owner_org in user_member_of_orgs):
             context['ignore_capacity_check'] = True
 
     query = logic.get_action('package_search')(context, search_dict)

ckanext/harvest/plugin.py
@@ -176,8 +176,6 @@ class Harvest(p.SingletonPlugin, DefaultDatasetForm, DefaultTranslation):
         return 'source/edit.html'
 
     def setup_template_variables(self, context, data_dict):
-        if hasattr(p.toolkit.c, 'pkg'):
-            p.toolkit.c.harvest_source = p.toolkit.c.pkg
 
         p.toolkit.c.dataset_type = DATASET_TYPE_NAME
 
@@ -303,6 +301,7 @@ class Harvest(p.SingletonPlugin, DefaultDatasetForm, DefaultTranslation):
                 'link_for_harvest_object': harvest_helpers.link_for_harvest_object,
                 'harvest_source_extra_fields': harvest_helpers.harvest_source_extra_fields,
                 'bootstrap_version': harvest_helpers.bootstrap_version,
+                'get_harvest_source': harvest_helpers.get_harvest_source,
                 }
 
     def dataset_facets(self, facets_dict, package_type):

ckanext/harvest/templates/snippets/job_error_summary.html
@@ -5,7 +5,7 @@ error_summary        - List of tuples with (message, count)
 
 Example:
 
-  {% snippet 'snippets/job_error_summary.html', summary=c.job.object_error_summary %}
+  {% snippet 'snippets/job_error_summary.html', summary=job.object_error_summary %}
 
 #}
 <table class="table table-striped table-bordered table-condensed harvest-error-summary">

ckanext/harvest/templates/snippets/source_item.html
@@ -11,7 +11,7 @@ show_organization - Boolean on whether to show the related organization
 
 Example:
 
-  {% snippet 'snippets/source_item.html', source=c.sources[0] %}
+  {% snippet 'snippets/source_item.html', source=sources[0] %}
 
 #}
 {% set truncate = truncate or 180 %}

ckanext/harvest/templates/snippets/source_list.html
@@ -11,11 +11,11 @@ truncate_title - The length to truncate the title to (default: 80).
 
 Example:
 
-  {% snippet 'snippets/sources_list.html', sources=c.sources %}
+  {% snippet 'snippets/sources_list.html', sources=sources %}
 
 #}
 {% if sources %}
-  <ul class="{{ list_class or 'dataset-list unstyled' }}">
+  <ul class="{{ list_class or 'dataset-list unstyled list-unstyled' }}">
     {% for source in sources %}
       {% snippet 'snippets/source_item.html', source=source, item_class=item_class, hide_resources=hide_resources, banner=banner, truncate=truncate, truncate_title=truncate_title, within_organization=within_organization %}
     {% endfor %}

ckanext/harvest/templates/source/about.html
@@ -2,12 +2,12 @@
 
 {% block primary_content_inner %}
   <section class="module-content">
-    <h1>{{ source.title or source.name }}</h1>
-    {% if source.notes %}
-      <p>{{ h.markdown_extract(source.notes)|urlize }}</p>
+    <h1>{{ harvest_source.title or harvest_source.name }}</h1>
+    {% if harvest_source.notes %}
+      <p>{{ h.markdown_extract(harvest_source.notes)|urlize }}</p>
     {% else %}
       <p class="empty">{{ _('There is no description for this harvest source') }}</p>
     {% endif %}
   </section>
-  {% snippet "package/snippets/additional_info.html", pkg_dict=source %}
+  {% snippet "package/snippets/additional_info.html", pkg_dict=harvest_source %}
 {% endblock %}

commit b206c4cf36248e8cf140fb7ed155d90bbb85dc5c

Fix abort job from ui
* Fix abort job from ui which was broken by commit
ed288dca63ffc6b2c3052c8e4aa3f14bf0525f9e

ckanext/harvest/controllers/view.py
@@ -233,6 +233,8 @@ class ViewController(BaseController):
 
     def abort_job(self, source, id):
         try:
+            context = {'model': model, 'user': c.user}
+            p.toolkit.get_action('harvest_job_abort')(context, {'id': id})
             h.flash_success(_('Harvest job stopped'))
 
         except p.toolkit.ObjectNotFound: