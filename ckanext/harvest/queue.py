09ec3c006dfc302360e32d8d133f6102b3b1df60
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2020-04-28 19:18:25 +0100

    Switch args around for redis.lrem function
    Reasons found here -
    https://github.com/andymccurdy/redis-py/issues/678
    
    To do with Redis vs StrictRedis

--- ckanext/harvest/queue.py
+++ ckanext/harvest/queue.py
@@ -203,7 +203,7 @@ class RedisPublisher(object):
         value = json.dumps(body)
         # remove if already there
         if self.routing_key == get_gather_routing_key():
-            self.redis.lrem(self.routing_key, 0, value)
+            self.redis.lrem(self.routing_key, value, 0)
         self.redis.rpush(self.routing_key, value)
 
     def close(self):
