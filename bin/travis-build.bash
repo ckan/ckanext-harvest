3c2c12e81af0da0baab5c3c7e91a6286e414de60
amercader <amercadero@gmail.com>
2020-08-27 14:39:31 +0200

    Reenable tests

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -12,6 +12,11 @@ fi
 
 export PYTHON_MAJOR_VERSION=${TRAVIS_PYTHON_VERSION%.*}
 
+
+echo "Installing the packages that CKAN requires..."
+sudo apt-get update -qq
+sudo apt-get install solr-jetty
+
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
@@ -22,11 +27,6 @@ else
     CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
     git checkout $CKAN_TAG
     echo "CKAN version: ${CKAN_TAG#ckan-}"
-fi
-
-if [ -f requirement-setuptools.txt ]
-then
-    pip install -r requirement-setuptools.txt
 fi
 
 python setup.py develop
@@ -41,12 +41,50 @@ fi
 pip install -r dev-requirements.txt
 cd -
 
-echo "Postgres versions running:"
-dpkg -l | grep postgresql
-sudo service postgresql status
+echo "Setting up Solr..."
+# solr is multicore for tests on ckan master now, but it's easier to run tests
+# on Travis single-core still.
+# see https://github.com/ckan/ckan/issues/2972
+sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
+
+echo "Creating the PostgreSQL user and database..."
+sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
+sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan_default;'
+
+echo "Initialising the database..."
+cd ckan
+if (( $CKAN_MINOR_VERSION >= 9 ))
+then
+    ckan -c test-core.ini db init
+else
+    paster db init -c test-core.ini
+fi
+cd -
+
+echo "Installing ckanext-harvest and its requirements..."
+pip install -r pip-requirements.txt
+pip install -r dev-requirements.txt
+
+python setup.py develop
 
 
+echo "Moving test.ini into a subdir... (because the core ini file is referenced as ../ckan/test-core.ini)"
+mkdir subdir
+mv test.ini subdir
+mv test-nose.ini subdir
 
-echo "Psycopg2 version installed:"
-pip show psycopg2
 
+if (( $CKAN_MINOR_VERSION >= 9 ))
+then
+    ckan -c subdir/test.ini harvester initdb
+else
+    paster harvester initdb -c subdir/test.ini
+fi
+
+
+echo "travis-build.bash is done."
--- bin/travis-run.bash
+++ bin/travis-run.bash
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "Done"
+if [ $CKANVERSION == 'master' ]
+then
+    export CKAN_MINOR_VERSION=100
+else
+    export CKAN_MINOR_VERSION=${CKANVERSION##*.}
+fi
+
+
+if (( $CKAN_MINOR_VERSION >= 9 ))
+then
+    pytest --ckan-ini=subdir/test.ini --cov=ckanext.harvest ckanext/harvest/tests
+else
+    nosetests --ckan --nologcapture --with-pylons=subdir/test-nose.ini --with-coverage --cover-package=ckanext.harvest --cover-inclusive --cover-erase --cover-tests ckanext/harvest/tests/nose
+fi
