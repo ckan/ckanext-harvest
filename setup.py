6c78efcb6b68077cf8a372bdaae12a50f83179b6
amercader <amercadero@gmail.com>
2020-08-31 21:02:35 +0200

    Fix tests, improve Travis setup
    * Run pytest tests only
    * Fix py2/py3 compat issues, factoryboy definitions
    * Run tests on focal
    * Use latest supported pg version for each ckan version
    * Use dockerized Solr

--- setup.py
+++ setup.py
@@ -39,11 +39,6 @@ setup(
             test_harvester2=ckanext.harvest.tests.test_queue2:MockHarvester
             test_action_harvester=ckanext.harvest.tests.test_action:MockHarvesterForActionTests
 
-            test_nose_harvester=ckanext.harvest.tests.nose.test_queue:MockHarvester
-            test_nose_harvester2=ckanext.harvest.tests.nose.test_queue2:MockHarvester
-            test_nose_action_harvester=ckanext.harvest.tests.nose.test_action:MockHarvesterForActionTests
-
-
         [paste.paster_command]
             harvester = ckanext.harvest.commands.harvester:Harvester
         [babel.extractors]
