150860e71a383a3afc7450904f5e826290d4b993
william dutton <will.dutt@gmail.com>
2023-02-15 15:25:26 +1000

    Allow User-Agent header to be added from config.

--- ckanext/harvest/harvesters/ckanharvester.py
+++ ckanext/harvest/harvesters/ckanharvester.py
@@ -36,6 +36,11 @@ class CKANHarvester(HarvesterBase):
     def _get_content(self, url):
 
         headers = {}
+
+        user_agent = self.config.get('user_agent')
+        if user_agent:
+            headers['User-Agent'] = user_agent
+
         api_key = self.config.get('api_key')
         if api_key:
             headers['Authorization'] = api_key
