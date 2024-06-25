0c5da80a8269f59d96291a6b518fc733cbf0fc59
amercader <amercadero@gmail.com>
2024-06-25 16:24:25 +0200

    Refactor harvest models
    Use the declarative way, and inherit from `BaseModel` in the toolkit,
    so tables get properly created and dropped in tests

--- ckanext/harvest/harvesters/base.py
+++ ckanext/harvest/harvesters/base.py
@@ -315,15 +315,11 @@ class HarvesterBase(SingletonPlugin):
                     return 'unchanged'
 
                 # Flag the other objects linking to this package as not current anymore
-                from ckanext.harvest.model import harvest_object_table
-                conn = Session.connection()
-                u = update(harvest_object_table)\
-                    .where(harvest_object_table.c.package_id == bindparam('b_package_id')) \
-                    .values(current=False)
-                conn.execute(u, b_package_id=new_package['id'])
+                Session.query(HarvestObject).filter(
+                    HarvestObject.package_id == new_package["id"]).update(
+                        {"current": False})
 
                 # Flag this as the current harvest object
-
                 harvest_object.package_id = new_package['id']
                 harvest_object.current = True
                 harvest_object.save()
