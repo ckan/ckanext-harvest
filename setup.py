698f21c05e4a30abc3b5ce386bf520389ea9b9c6
amercader <amercadero@gmail.com>
2024-09-13 15:57:40 +0200

    First iteration of pyproject.toml

--- setup.py
+++ setup.py
@@ -1,47 +1,6 @@
-from setuptools import setup, find_packages
-
-version = '1.5.6'
+from setuptools import setup
 
 setup(
-    name='ckanext-harvest',
-    version=version,
-    description="Harvesting interface plugin for CKAN",
-    long_description="""\
-    """,
-    classifiers=[],
-    keywords='',
-    author='CKAN',
-    author_email='ckan@okfn.org',
-    url='https://github.com/ckan/ckanext-harvest',
-    license='AGPL',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-            # dependencies are specified in requirements.txt
-            # instead of here
-    ],
-    tests_require=[
-        'nose',
-        'mock',
-    ],
-    test_suite='nose.collector',
-    entry_points="""
-        [ckan.plugins]
-            # Add plugins here, eg
-            harvest=ckanext.harvest.plugin:Harvest
-            ckan_harvester=ckanext.harvest.harvesters:CKANHarvester
-
-            # Test plugins
-
-            test_harvester=ckanext.harvest.tests.test_queue:MockHarvester
-            test_harvester2=ckanext.harvest.tests.test_queue2:MockHarvester
-            test_action_harvester=ckanext.harvest.tests.test_action:MockHarvesterForActionTests
-
-        [babel.extractors]
-            ckan = ckan.lib.extract:extract_ckan
-    """,
     message_extractors={
         'ckanext': [
             ('**.py', 'python', None),
