a77092439b2016b6aae1667a5d2f8551fff96b67
amercader <amercadero@gmail.com>
2026-08-11 14:01:55 +0200

    Remove unnecessary namespace declarations

--- ckanext/harvest/logic/__init__.py
+++ ckanext/harvest/logic/__init__.py
@@ -1,11 +1,3 @@
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    import pkgutil
-    __path__ = pkgutil.extend_path(__path__, __name__)
-
-
 class HarvestJobExists(Exception):
     pass
 
